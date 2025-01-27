c6cef855a12a862348e1093343cfbc5c58182437
Pedro DÃíaz <diazpd2006@gmail.com>
2025-01-27 18:00:15 +0100

    Cambios06

---
+++ TEMA05/Ejercicios05_PedroDiaz/src/ejercicio01/Nota.java
@@ -0,0 +1,42 @@
+package ejercicio01;
+
+public class Nota {
+
+	private int id;
+	private String titulo;
+	private String contenido;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getContenido() {
+		return contenido;
+	}
+	public void setContenido(String contenido) {
+		this.contenido = contenido;
+	}
+	@Override
+	public String toString() {
+		return "Nota [id=" + id + ", titulo=" + titulo + ", contenido=" + contenido + "]";
+	}
+	public Nota(int id, String titulo, String contenido) {
+		super();
+		this.id = id;
+		this.titulo = titulo;
+		this.contenido = contenido;
+	}
+	
+	
+	
+	
+
+	
+}
