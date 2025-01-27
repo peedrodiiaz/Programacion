24945197acfe45f2b72e6e82d3dc62fedb7792bf
Pedro Díaz Garduño <diaz.gaped24@triana.salesianos.edu>
2025-01-27 13:20:48 +0100

    cambios 3

--- UD05Ejemplo/src/ejemplo01/Principal.java
+++ UD05Ejemplo/src/ejemplo01/Principal.java
@@ -11,15 +11,27 @@ public class Principal {
 		
 		Producto p1 = new Producto(2.33, false, "Patata");
 		Producto p2 = new Producto(3.5, true, "Healado");
+		Producto p3 = new Producto(1.5, true, "FrioPies");
+		Producto p4 = new Producto(5.5, true, "Sandía");
 		
 		List <Producto> listado=  new ArrayList<Producto>();
 		
 		listado.add(p2);
-		
+		listado.add(p3);
+		listado.add(p4);
 		System.out.println(listado);
+		System.out.println("Tamaño: "+listado.size());
+	
+		listado.add(p1);
+		System.out.println("Tamaño nuevo "+listado.size());
+		
+		System.out.println(listado.remove(p3));
+		System.out.println(listado.remove(2));
+		System.out.println("Tamaño nuevo "+listado.size());
+
 		
 		
-		System.out.println("HOLA");
+		
 	}
 
 }
